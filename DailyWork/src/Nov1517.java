0af64729b3a11a88f32c5ac00bc03adfc26793b8
Matteo Cordray <matteo.cordray@hpisd.org>
2017-11-15 08:19:07 -0600

    edit nov

---
+++ DailyWork/src/Nov1517.java
@@ -0,0 +1,18 @@
+
+public class Nov1517 {
+
+	public static void main(String[] args){
+		
+	}
+	
+	public static int method(int s){
+		
+		int sum = 0;
+		
+		for(int i = 0; i < s / 2; i++){
+			sum += (i / 2);
+		}
+		
+		return 0;
+	}
+}
