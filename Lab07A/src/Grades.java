66f92e220329e6c23bd7df054f4e77850a06c646
Matteo Cordray <matteo.cordray@hpisd.org>
2018-01-09 15:04:12 -0600

    Edit 2

---
+++ Lab07A/src/Grades.java
@@ -0,0 +1,90 @@
+//Name -
+//Date -
+//Lab  -
+ 
+import java.util.*;
+import static java.lang.System.*;
+
+public class Grades
+{
+	private double[] grades;
+
+	//**finish this initialization constructor
+	public Grades(double[] nums)
+	{
+		
+	}
+
+	//**finish this mutator
+	public void setGrades(double[] nums)
+	{
+		
+	}
+
+	
+	//**this method will return the total of all the elements
+	//  in the array	
+	public double getTotal()
+	{
+		double total = 0.0;
+	
+		
+		return total;
+	}
+	
+	//**this method will return the average of all the elements
+	//  in the array; use the getTotal() method above
+	public double getAverage()
+	{
+		double average=0.0;
+
+
+
+		return average;
+	}
+	
+	//**this method will return the average with the lowest grade dropped
+	public double getAverageDropLowest()
+	{
+		return 0;
+	}
+	
+	//**this method will return the maximum of all the elements
+	//  in the array	
+	public double getHighest()
+	{
+		double max=0.0;
+		
+
+		
+		return max;
+	}
+	
+	//**this method will return the minimum of all the elements
+	//  in the array	
+	public double getLowest()
+	{
+		double min=0;
+		
+
+		
+		return min;
+	}
+
+
+	public double roundTwo(double x)
+	{
+		return 0;
+	}
+	
+	public String toString( )
+	{
+		String output="";
+		
+	
+		
+		return output;
+	}
+}
+
+
--- Lab07A/src/Lab07A.java
+++ Lab07A/src/Lab07A.java
@@ -1,4 +1,27 @@
+//Matteo Cordray
+//Date - 1/9/18
+//Lab  - Lab07A
 
-public class Lab07A {
-	
+import java.util.*;
+import static java.lang.System.*;
+
+//this runner is finished
+public class Lab07A
+{
+	public static void main( String args[] )
+	{
+		Grades test = new Grades(new double[]{100,90,85,72.5,95.6});
+		System.out.println(test);
+
+		test.setGrades(new double[]{50.0,100.0,80.0});
+		System.out.println(test);
+
+		test.setGrades(new double[]{93.4,-90.0,90.0});
+		System.out.println(test);
+
+		test.setGrades(new double[]{1,2,3,4,5,6,7,8,9});
+		System.out.println(test);
+		
+		out.println ("\n\nProgram completed by:  your name here");
+		}
 }
